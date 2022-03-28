99fb10d4429796840d7a632faf4ac9e26d3148e8
zakaria444 <elismailizakariae@gmail.com>
2022-03-28 17:29:05 +0000

    signup and signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from "react-router-dom"
+
+import Signin from './Authenticated/Signin';
+import Signup from './Authenticated/Signup';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Routes>
+  <Route path='/signin' element={<Signin/>} />
+  <Route path='/signup' element={<Signup/>} />
+
+</Routes>
+
     </div>
   );
 }
