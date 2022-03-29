97a9d3fe08a35c6904003caa35e4fd0b4ec6951a
zakaria444 <elismailizakariae@gmail.com>
2022-03-29 16:42:57 +0000

    ajouter total prix command

--- Back-End/controllers/command/Command.js
+++ Back-End/controllers/command/Command.js
@@ -1,60 +1,50 @@
-const Command = require("../../models/Command")
-const CommandRepas = require("../../models/Command-reapas")
-
+const Command = require("../../models/Command");
+const CommandRepas = require("../../models/Command-reapas");
 
 const AddCommand = async (req, res) => {
-
   try {
-    const user_id= req.params.userId
-    const adress = req.body.adress
+    const user_id = req.params.userId;
+    const adress = req.body.adress;
 
     const newCommand = new Command({
       adress: adress,
-      user_id:user_id,
-    })
-  
-    const saveCommand = await newCommand.save()
+      user_id: user_id,
+    });
+
+    const saveCommand = await newCommand.save();
     // res.status(201).json({ success: true, data: saveCommand })
 
-    if(saveCommand){
-      const products= req.body.products
+    if (saveCommand) {
+      const products = req.body.products;
+      let total = 0;
+      products.map(async (reapas) => {
+        let subTotal = reapas.prix * reapas.quntity;
+        const newCommandrepas = new CommandRepas({
+          command_id: saveCommand._id,
+          repas_id: reapas.reapas_id,
+          prix: reapas.prix,
+          quntity: reapas.quntity,
+          total: subTotal,
+        });
 
-      products.map(async(reapas)=>{
-      const newCommandrepas = new CommandRepas({
-       
-        command_id:saveCommand._id ,
-        repas_id:reapas.reapas_id,
-        prix : reapas.prix,
-        quntity:reapas.quntity,
-        total:reapas.prix * reapas.quntity
+        total += subTotal;
+        await newCommandrepas.save();
       });
-      await newCommandrepas.save()
+
       await Command.updateOne({ _id: saveCommand._id  }, {
         $set: {
-          total: reapas.prix * reapas.quntity,
+          total: total,
         }
       })
-    })
-
-
     }
-  
-    
-    res.status(201).json({ success: true, data: "yeeeeeeeeeees" })
 
+    res.status(201).json({ success: true, data: "yeeeeeeeeeees" });
   } catch (error) {
-   
-        console.log(error)
-        res.status(404).json({ success: false, data: [], error: error })
+    console.log(error);
+    res.status(404).json({ success: false, data: [], error: error });
   }
-
-}
-
-
-
+};
 
 module.exports = {
-    AddCommand,
-    
-
+  AddCommand,
 };
