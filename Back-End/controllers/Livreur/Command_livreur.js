326a179a53434af5869df6349d6ca18debc75723
zakaria444 <elismailizakariae@gmail.com>
2022-04-02 12:46:42 +0000

    populate

--- Back-End/controllers/Livreur/Command_livreur.js
+++ Back-End/controllers/Livreur/Command_livreur.js
@@ -6,8 +6,7 @@ const show_command_repas = async (req, res) => {
 
   try {
     const command_repas = await CommandRepas.find({ command_id: Idcommand })
-      .populate("command_id")
-      .populate("repas_id");
+      .populate("command_id").populate("repas_id");
     res.status(200).json({ success: true, data: command_repas });
   } catch (error) {
     res.status(404).json({ success: false, data: [], error: error });
