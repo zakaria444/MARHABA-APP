5efa6159998688892559ad6b17ef0961c8156e5a
zakaria444 <elismailizakariae@gmail.com>
2022-04-19 10:02:40 +0000

    💹Action Redux Auth

---
+++ frontend/src/redux/actions/AuthAction.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+
+const AuthActionType = {
+  REGISTER_SUCCESS: "REGISTER_SUCCESS",
+  REGISTER_FAIL: "REGISTER_FAIL",
+  LOGOUT_SUCCESS: "LOGOUT_SUCCESS",
+  LOGOUT_FAIL: "LOGOUT_FAIL",
+  LOGIN_SUCCESS: "LOGIN_SUCCESS",
+  LOGIN_FAIL: "LOGIN_FAIL",
+};
+
+const RegisterAuthAction = (userState, history, setErrorHandler) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post("/register", userState);
+      const { data } = res;
+      dispatch({ type: AuthActionType.REGISTER_SUCCESS, payload: data });
+      history.push("/");
+    } catch (error) {
+      if (error.response) {
+        dispatch({
+          type: AuthActionType.REGISTER_FAIL,
+          payload: error.response.data.message,
+        });
+        setErrorHandler({
+          hasError: true,
+          message: error.response.data.message,
+        });
+      }
+    }
+  };
+};
+
+const LoginAuthAction = (loginState, history, setErrorHandler) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post("/authenticate", loginState);
+      const { data } = res;
+      dispatch({ type: AuthActionType.LOGIN_SUCCESS, payload: data });
+      history.push("/");
+    } catch (error) {
+      if (error.response) {
+        dispatch({
+          type: AuthActionType.LOGIN_FAIL,
+          payload: error.response.data.message,
+        });
+      }
+      setErrorHandler({ hasError: true, message: error.response.data.message });
+    }
+  };
+};
+
+const LogOutAuthAction = (history) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.get("/logout");
+      const { data } = res;
+      dispatch({
+        type: AuthActionType.LOGOUT_SUCCESS,
+        payload: data.message,
+      });
+      history.push("/");
+    } catch (error) {
+      if (error.response) {
+        dispatch({
+          type: AuthActionType.LOGOUT_FAIL,
+          payload: error.response.data.message,
+        });
+      }
+    }
+  };
+};
+
+export {
+  RegisterAuthAction,
+  AuthActionType,
+  LogOutAuthAction,
+  LoginAuthAction,
+};
